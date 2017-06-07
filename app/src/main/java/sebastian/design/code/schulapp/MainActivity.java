29ef8b8a3b31e4a961913886b45f31aae227e86f
Your Name <laptop@codecamp.design>
2017-06-07 18:30:52 +0200

    fragments fuer einzelne screens hinzugefügt

--- app/src/main/java/sebastian/design/code/schulapp/MainActivity.java
+++ app/src/main/java/sebastian/design/code/schulapp/MainActivity.java
@@ -1,19 +1,24 @@
 package sebastian.design.code.schulapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SecondFragment.OnFragmentInteractionListener {
+    private static final String TAG = "MainActivity";
 
+    FrameLayout frameLayout;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -22,12 +27,20 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_Start:
+                    final SecondFragment secondFragment = new SecondFragment();
+                    changeFragment(secondFragment);
                     return true;
                 case R.id.navigation_dashboard:
+                    final SecondFragment dashboardFragment = new SecondFragment();
+                    changeFragment(dashboardFragment);
                     return true;
                 case R.id.navigation_notifications:
+                    final SecondFragment notificationFragment = new SecondFragment();
+                    changeFragment(notificationFragment);
                     return true;
                 case R.id.navigation_Einstellungen:
+                    final SecondFragment settingsFragment = new SecondFragment();
+                    changeFragment(settingsFragment);
                     return true;
             }
             return false;
@@ -35,10 +48,19 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+    private void changeFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = (FrameLayout) findViewById(R.id.fragment_container);
+
+
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -50,4 +72,8 @@ public class MainActivity extends AppCompatActivity {
         FirebaseMessaging.getInstance().subscribeToTopic("news");
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.v(TAG, "onFragmentInteraction()");
+    }
 }
