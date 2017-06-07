41515d53260d04dec68584b9128959070c144b9b
Ascanius <adrian-beckmann@t-online.de>
2017-06-07 14:46:32 +0200

    impoved design code

--- app/src/main/java/sebastian/design/code/schulapp/MainActivity.java
+++ app/src/main/java/sebastian/design/code/schulapp/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +21,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_Start:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.title_Startseite);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_Vertretungsplan);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_Schulaufgabenplan);
                     return true;
                 case R.id.navigation_Einstellungen:
                     mTextMessage.setText(R.string.title_Einstellungen);
@@ -46,4 +49,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
 
