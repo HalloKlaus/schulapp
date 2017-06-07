9390cddf46ef54c4b9acee629324147061a0c55f
Ascanius <adrian-beckmann@t-online.de>
2017-06-07 11:12:02 +0200

    Changed Menu Structure

--- app/src/main/java/sebastian/design/code/tutorial/MainActivity.java
+++ app/src/main/java/sebastian/design/code/tutorial/MainActivity.java
@@ -1,28 +1,49 @@
 package sebastian.design.code.tutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_Start:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_Einstellungen:
+                    mTextMessage.setText(R.string.title_Einstellungen);
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
-    protected void sendMessage() {
-
-        Log.println(Log.WARN,"Hallo","Hallo");
-    }
-
-    public void hallo(){}
-
-    public void derdadrübenisdumm(){
-
-    }
-    public void teamaufteilung() {}
 
 }
+
